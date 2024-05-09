1c0964d3255367d831b924d42b329908a80b931f
Milasses <gavrelyuk.milana@mail.ru>
2024-05-10 04:08:34 +0500

    Update technology-15.py

--- technology-15.py
+++ technology-15.py
@@ -18,6 +18,8 @@ st.markdown("<h1 style='text-align: center; color: green; font-family: Times New
 #Добавление фона сайта
 st.image(image)
 
+# Данные для каждого региона
+regions = ['Казахстан', 'Кыргызстан', 'Таджикистан', 'Узбекистан']
 kaz_values = [0.0737473506983265, 0.044529239425859325, 0.07208697980276833, 0.09025550050680399]
 uzb_values = [0.09872602667454446, 0.12482079148104783, 0.1033934827101725, 0.16342414956949367]
 tjk_values = [0.13234311608076732, 0.11086727498562164, 0.19598527440470287, 0.23921461895440915]
@@ -28,26 +30,25 @@ years = [2014, 2015, 2016, 2017]
 st.markdown("<h1 style='text-align: center; color: green; font-family: Times New Roman; font-size: 20px; '> Для того чтобы увидеть график с определенной страной нажмите на соответствующую кнопку</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; font-size: 20px; '>←--------------------------------</h1>", unsafe_allow_html=True)
 
-#Кнопки
-page = st.sidebar.radio('Выберите страницу', ['Главная', 'Казахстан', 'Таджикистан', 'Кыргызстан', 'Узбекистан', 'Центральная Азия'])
+# Отображение графика в зависимости от выбранного региона
+if selected_region == 'Казахстан':
+    st.header('График для выбранного региона: Казахстан')
+    st.line_chart(dict(zip(years, kaz_values)))
+elif selected_region == 'Кыргызстан':
+    st.header('График для выбранного региона: Кыргызстан')
+    st.line_chart(dict(zip(years, kgz_values)))
+elif selected_region == 'Таджикистан':
+    st.header('График для выбранного региона: Таджикистан')
+    st.line_chart(dict(zip(years, tjk_values)))
+elif selected_region == 'Узбекистан':
+    st.header('График для выбранного региона: Узбекистан')
+    st.line_chart(dict(zip(years, uzb_values)))
 
-#Функция для создания графиков 
-def plot_country_graph(country, values, years):
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(years, values, marker='o', linestyle='----')
-    ax.set_title(country)
-    ax.set_xticks(years)
-    ax.set_yticks(np.arange(0, 0.31, 0.05))
-    ax.grid(True)
-    st.pyplot(fig)
+# Отображение графика для всех стран
+all_countries_chart = st.line_chart({
+    'Казахстан': kaz_values,
+    'Кыргызстан': kgz_values,
+    'Таджикистан': tjk_values,
+    'Узбекистан': uzb_values
+})
 
-if kaz_button:
-    plot_country_graph('Казахстан', kaz_values, years, page)
-elif uzb_button:
-    plot_country_graph('Узбекистан', uzb_values, years, page)
-elif tjk_button:
-    plot_country_graph('Таджикистан', tjk_values, years, page)
-elif kgz_button:
-    plot_country_graph('Кыргызстан', kgz_values, years, page)
-    
-#Отображение графика для всех стран

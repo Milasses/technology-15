aa63f0291edf2b841b56e1a41a42edeaefc3f182
Milasses <gavrelyuk.milana@mail.ru>
2024-05-10 00:38:16 +0500

    Update technology-15.py

--- technology-15.py
+++ technology-15.py
@@ -12,3 +12,26 @@ image4 = Image.open("кыргызстан.jpg")
 image5 = Image.open("общий график.jpg")
 
 st.image(image)
+
+Prob_Mod_Sev_kaz_values = [0.0737473506983265, 0.044529239425859325, 0.07208697980276833, 0.09025550050680399]
+Prob_Mod_Sev_uzb_values = [0.09872602667454446, 0.12482079148104783, 0.1033934827101725, 0.16342414956949367]
+Prob_Mod_Sev_tjk_values = [0.13234311608076732, 0.11086727498562164, 0.19598527440470287, 0.23921461895440915]
+Prob_Mod_Sev_kgz_values = [0.1875365079274166, 0.21059007745097164, 0.19581301002148638, 0.19449654750600165]
+years = [2014, 2015, 2016, 2017]
+
+#Функция для создания графиков 
+def plot_country_graph(country, values, years):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(years, values, marker='o', linestyle='-')
+    ax.set_title(country)
+    ax.set_xticks(years)
+    ax.set_yticks(np.arange(0, 0.31, 0.05))
+    ax.grid(True)
+    st.pyplot(fig)
+
+# Ввод теста
+st.markdown("<h1 style='text-align: on right; color: green; font-family: Times New Roman;'>Данная страница представляет информацию продовольственной безопасности центральной Азии</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: on right; color: green; font-family: Times New Roman;'> Для того чтобы увидеть график с определенной страной нажмите на соответствующую кнопку</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>←--------------------------------</h1>", unsafe_allow_html=True)
+
+

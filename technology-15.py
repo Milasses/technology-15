bd5835249e845e4b1daeb4d5763e891f18169a09
Milasses <gavrelyuk.milana@mail.ru>
2024-05-09 23:40:03 +0500

    Update technology-15.py

--- technology-15.py
+++ technology-15.py
@@ -4,6 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 #Добавление изображений
-image5 = Image.open("119-full.jpg")
+image = Image.open("119-full.jpg")
 
 st.image(image)

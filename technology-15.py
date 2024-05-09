69a8dad434f33e1553cf7814c201e2f794845760
Milasses <gavrelyuk.milana@mail.ru>
2024-05-10 00:09:40 +0500

    Update technology-15.py

--- technology-15.py
+++ technology-15.py
@@ -4,6 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 #Добавление изображений
-image = Image.open("центральная азия.pnd")
+image = st.image("центральная азия.png", caption='Изображение PNG', use_column_width=True)
 
 st.image(image)

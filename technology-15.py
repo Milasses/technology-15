101a3aaa09f1bb32dd4d60b19f9ea6c07e3051c4
Milasses <gavrelyuk.milana@mail.ru>
2024-05-10 00:17:33 +0500

    Update technology-15.py

--- technology-15.py
+++ technology-15.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 #Добавление изображений
-image = st.image("центральная азия.png", caption='Изображение PNG', use_column_width=True)
+image = Image.open("центральная азия.jpg")
+image1 = Image.open("казахстан.jpg")
+image2 = Image.open("узбекистан.jpg")
+image3 = Image.open("таджикистан.jpg")
+image4 = Image.open("кыргызстан.jpg")
+image5 = Image.open("общий график.jpg")
 
 st.image(image)

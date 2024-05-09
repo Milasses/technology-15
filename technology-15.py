ef386a7e0ec6fdc37910b26b3413768dc0a74a3a
Milasses <gavrelyuk.milana@mail.ru>
2024-05-09 23:31:57 +0500

    Update technology-15.py

--- technology-15.py
+++ technology-15.py
@@ -4,13 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 #Добавление изображений
+background_image = "искендерькульtourister.ru.jpg"
+
+# Устанавливаем фоновое изображение
 st.markdown(
-    """
+    f"""
     <style>
-    .reportview-container {
-        background: url("искендерькульtourister.jpg");
+    .reportview-container {{
+        background: url("{background_image}");
         background-size: cover;
-    }
+    }}
     </style>
     """,
     unsafe_allow_html=True
